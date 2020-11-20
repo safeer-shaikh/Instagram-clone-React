b82d0cd74cf1544833f67f7c89ad55b14de4f47e
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-11-20 15:57:05 +0500

    Header Completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import { Link } from 'react-router-dom'
 import logoImage from '../../images/logo.png'
 
 export default function Header(){
@@ -11,9 +12,28 @@ export default function Header(){
                         <img src={logoImage} />
                     </div>
                     <div className='col-md-4 input'>
-                        <input type='text' placeholder='Search' name='search'/>
+                        <div style={{display: "flex"}}>
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                            <input type='text' placeholder='Search' name='search'/>
+                        </div>
                     </div>
-                    <div className='col-md-4'></div>
+                    <div className='col-sm-3 icons'>
+                        <div>
+                            <button><i style={{fontSize: 30, }} class="fa fa-home"></i></button>
+                        </div>
+                        <div>
+                            <button><i style={{fontSize: 22, marginTop: 5,color: "#bdbdbd",}} class="fa fa-paper-plane"></i></button>
+                        </div>
+                        <div>
+                            <button><i style={{fontSize: 27,marginTop: 4,color: "#bdbdbd",}} class="fa fa-compass"></i></button>
+                        </div>
+                        <div>
+                            <button><i style={{fontSize: 24,marginTop: 5,color: "#bdbdbd",}} class="fa fa-heart"></i></button>
+                        </div>
+                        <div>
+                            <button><img /></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
