45f9c87100494ea71c607f2aadf2fa1b5fa72429
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-11-22 19:51:08 +0500

    Login page complete

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import './login.css'
+import {Link} from 'react-router-dom'
 import Footer from '../../components/Footer/Footer'
 import mobileImage from '../../images/mobile.png'
 import logoImage from '../../images/logo.png'
+import googlePlay from '../../images/googlestore.png'
+import appStore from '../../images/appstore.png'
 
 class Login extends React.Component{
+    constructor(){
+        super()
+        this.state ={
+            show: true,
+        }
+    }
     render(){
         return(
             <div>
                 <div className='container' style={{backgroundColor: "transparent", maxWidth: '935px'}}>
-                    <div className='body'>
+                    <div className='body1'>
                         <div className='left_div'>
                             <img src={mobileImage} alt='mobile'/>
                         </div>
-                        <div className='right_div container' style={{maxWidth: 350, marginLeft: "-0", height: 230}}>
-                            <div className='right_sub_div'>
+                        {this.state.show ?
+                        <div className='right_div1 container' style={{maxWidth: 350, marginLeft: "-0", height: 230}}>
+                            <div className='right_sub_div1'>
                                 <center>
                                     <div className='logo'>
                                         <img src={logoImage} alt='logo' />
@@ -28,7 +38,7 @@ class Login extends React.Component{
                                     <div className='create_account'>
                                         <span>
                                             Want to create an Account?
-                                            <button>
+                                            <button onClick={()=>this.setState({show: false})}>
                                                 Create Account
                                             </button>
                                         </span>
@@ -36,6 +46,58 @@ class Login extends React.Component{
                                 </center>
                             </div>
                         </div>
+                        :
+                        <div className='right_div2'>
+                            <div className='right_sub_div2 container'>
+                                <center>
+                                    <form>
+                                        <div className='logo1'>
+                                            <img src={logoImage} alt='logo' />
+                                        </div>
+                                        <div className='inputs'>
+                                            <input type='text' placeholder='Phone number, username, or email' name='username' required/>
+                                        </div>
+                                        <div className='inputs'>
+                                            <input type='password' placeholder='Password' name='password' required/>
+                                        </div>
+                                        <div className='login_btn'>
+                                            <button type='submit' >Log In</button>
+                                        </div>
+                                        <div className='partitionOR'>
+                                            <span>____________</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <span style={{color: "gray",}}>OR</span>&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <span>____________</span>
+                                        </div>
+                                        <div className='to_facebook_btn' style={{marginTop: 20}}>
+                                            <button onClick={()=>this.setState({show: true})}><i class="fa fa-facebook-square"></i>Log in with Facebook</button>
+                                        </div>
+                                        <div className='forgot_pass' style={{marginBottom: 20}}>
+                                            <Link>Forgot Password?</Link>
+                                        </div>
+                                    </form>
+                                </center>
+                            </div>
+                            <div className='container-tm'>
+                                <center>
+                                    <div>
+                                        <span>Don't have an account?</span>
+                                        <Link>Sign up</Link>
+                                    </div>
+                                </center>
+                            </div>
+                            <div>
+                                <center>
+                                    <div>
+                                        <span style={{fontSize: 13}}>Get the app.</span>
+                                    </div>
+                                    <div className='store_btn' style={{display: "flex", justifyContent: "center"}}>
+                                        <button><img src={appStore} width='140px' /></button>
+                                        <button><img src={googlePlay} width='140px' /></button>
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    }
                     </div>
                 </div>
                 <Footer />
