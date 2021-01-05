29d0198e0f1858388c0a8b625cb9811dce640623
safeer-shaikh <safeersohail210.ss@gmail.com>
2021-01-06 00:20:40 +0500

    initiated chat section UI

--- src/containers/Chats/Chats.js
+++ src/containers/Chats/Chats.js
@@ -6,15 +6,58 @@ import {connect} from 'react-redux'
 import { Link } from 'react-router-dom'
 
 class Chats extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            users: [],
+        }
+    }
 
     componentDidMount(){
-        this.props.get_users()
+        this.props.get_users();
+        this.setState({users: this.props.users})
     }
 
     render(){
+        let user = this.props.current_user
+        console.log(this.props.users)
+        console.log(this.state.users)
         return(
             <div>
                 <Header />
+                <div style={{border: '1px solid #dbdbdb', marginTop: 20, padding: 0}} className='container'>
+                    <div className='main'>
+                        <section className='left_section'>
+                            <div>
+                                <p>
+                                    <Link to='#'>{user.name}</Link>
+                                </p>
+                            </div>
+                            <div>
+                                <ul>
+                                    {
+                                        this.state.users.map((v,i)=>{
+                                            return(
+                                                v.uid !== user.uid &&
+                                                <button key={i}>
+                                                    <li>
+                                                        <img src={v.profile} />
+                                                        <span>{v.name}</span>
+                                                    </li>
+                                                </button>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        </section>
+                        <section className='right_section'>
+                            <div>
+
+                            </div>
+                        </section>
+                    </div>
+                </div>
             </div>
         )
     }
